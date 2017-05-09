5e18e665624f95076bd5e637d69d717b97b4a7d7
wcervant <wcervant@salviadeveloppement.com>
2017-05-09 18:59:38 +0200

    Create windows service.

---
+++ CalculatorService/ICalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.ServiceModel;
+
+namespace CalculatorService
+{
+    // Define a service contract.
+    [ServiceContract(Namespace = "Microsoft.Samples.ConfigSimplification")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
--- Service/Service.cs
+++ Service/Service.cs
@@ -4,68 +4,19 @@
 
 using System;
 using System.ServiceModel;
+using  CalculatorService;
 
 namespace Microsoft.Samples.ConfigSimplification
 {
-    // Define a service contract.
-    [ServiceContract(Namespace = "Microsoft.Samples.ConfigSimplification")]
-    public interface ICalculator
-    {
-        [OperationContract]
-        double Add(double n1, double n2);
 
-        [OperationContract]
-        double Subtract(double n1, double n2);
-
-        [OperationContract]
-        double Multiply(double n1, double n2);
-
-        [OperationContract]
-        double Divide(double n1, double n2);
-    }
-
-    // Service class which implements the service contract.
     // Added code to write output to the console window
-    public class CalculatorService : ICalculator
+    public static class CalculatorProgramm 
     {
-        public double Add(double n1, double n2)
-        {
-            var result = n1 + n2;
-            Console.WriteLine("Received Add({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Subtract(double n1, double n2)
-        {
-            var result = n1 - n2;
-            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Multiply(double n1, double n2)
-        {
-            var result = n1 * n2;
-            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Divide(double n1, double n2)
-        {
-            var result = n1 / n2;
-            Console.WriteLine("Received Divide({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-
         // Host the service within this EXE console application.
         public static void Main()
         {
             // Create a ServiceHost for the CalculatorService type.
-            using (var serviceHost = new ServiceHost(typeof(CalculatorService),
+            using (var serviceHost = new ServiceHost(typeof(CalculatorService.CalculatorService),
                 new Uri("http://localhost:8000/ServiceModelSamples/Service.svc")))
             {
                 // Open the ServiceHost to create listeners and start listening for messages.
