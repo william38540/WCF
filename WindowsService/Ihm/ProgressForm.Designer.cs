016f37cb04b7da665840a5dea9889df90c49d52f
wcervant <wcervant@salviadeveloppement.com>
2017-05-10 17:51:45 +0200

    Add ProgressForm

---
+++ WindowsService/Ihm/ProgressForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Progress
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressForm));
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonCancel.Location = new System.Drawing.Point(122, 61);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Annuler";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(76, 20);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(98, 13);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Veuillez patientier...";
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(319, 96);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ProgressForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Opération en cours";
+            this.Load += new System.EventHandler(this.ProgressForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
---
+++ WindowsService/Ihm/ProgressForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Progress
+{
+    /// <summary>
+    /// Simple progress form.
+    /// </summary>
+    public partial class ProgressForm : Form
+    {
+        /// <summary>
+        /// Will be passed to the background worker.
+        /// </summary>
+        public object Argument { get; set; }
+        /// <summary>
+        /// Background worker's result.
+        /// You may also check ShowDialog return value
+        /// to know how the background worker finished.
+        /// </summary>
+        public RunWorkerCompletedEventArgs Result { get; private set; }
+        /// <summary>
+        /// True if the user clicked the Cancel button
+        /// and the background worker is still running.
+        /// </summary>
+        public bool CancellationPending
+        {
+            get { return worker.CancellationPending; }
+        }
+        /// <summary>
+        /// Text displayed once the Cancel button is clicked.
+        /// </summary>
+        public string CancellingText { get; set; }
+        /// <summary>
+        /// Default status text.
+        /// </summary>
+        public string DefaultStatusText { get; set; }
+
+        /// <summary>
+        /// Delegate for the DoWork event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Contains the event data.</param>
+        public delegate void DoWorkEventHandler(ProgressForm sender, DoWorkEventArgs e);
+        /// <summary>
+        /// Occurs when the background worker starts.
+        /// </summary>
+        public event DoWorkEventHandler DoWork;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ProgressForm()
+        {
+            InitializeComponent();
+
+            CancellingText = "Annulation en cours...";
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new System.ComponentModel.DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+        }
+
+        private void ProgressForm_Load(object sender, EventArgs e)
+        {
+            //reset to defaults just in case the user wants to reuse the form
+            Result = null;
+            buttonCancel.Enabled = true;
+            labelStatus.Text = DefaultStatusText;
+            //start the background worker as soon as the form is loaded
+            worker.RunWorkerAsync(Argument);
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            //notify the background worker we want to cancel
+            worker.CancelAsync();
+            //disable the cancel button and change the status text
+            buttonCancel.Enabled = false;
+            labelStatus.Text = CancellingText;
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //the background worker started
+            //let's call the user's event handler
+            if (DoWork != null)
+                DoWork(this, e);
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //the background worker completed
+            //keep the resul and close the form
+            Result = e;
+            if (e.Error != null)
+                DialogResult = DialogResult.Abort;
+            else if (e.Cancelled)
+                DialogResult = DialogResult.Cancel;
+            else
+                DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        BackgroundWorker worker;
+    }
+}
