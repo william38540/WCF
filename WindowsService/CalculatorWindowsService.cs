dde15a20d2f0c6bd7b55858cce8812ebbce349b7
wcervant <wcervant@salviadeveloppement.com>
2017-05-10 17:50:51 +0200

    Add Service

--- WindowsService/CalculatorWindowsService.cs
+++ WindowsService/CalculatorWindowsService.cs
@@ -1,74 +1,330 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ComponentModel;
-using System.ServiceModel;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Configuration;
-using System.Configuration.Install;
+using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace WindowsService
 {
-    // Provide the ProjectInstaller class which allows 
-    // the service to be installed by the Installutil.exe tool
-    [RunInstaller(true)]
-    public class ProjectInstaller : Installer
+    internal class CalculatorWindowsService
     {
-        private ServiceProcessInstaller process;
-        private ServiceInstaller service;
-
-        public ProjectInstaller()
-        {
-            process = new ServiceProcessInstaller();
-            process.Account = ServiceAccount.LocalSystem;
-            service = new ServiceInstaller();
-            service.ServiceName = "WCFWindowsServiceSample";
-            Installers.Add(process);
-            Installers.Add(service);
-        }
-    }
-
-    class CalculatorWindowsService : ServiceBase
-    {
-        public ServiceHost serviceHost = null;
-        public CalculatorWindowsService()
-        {
-            // Name the Windows Service
-            ServiceName = "WCFWindowsServiceSample";
-        }
+        private static bool _isInstalled;
+        private static readonly string ServiceName = "WCFWindowsServiceSample";
+        private static readonly string DisplayName = @"WCF Windows Service Sample";
+        private static readonly string Comment = @"Create a WCF windows service.";
 
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        ///     Point d'entrée principal de l'application.
         /// </summary>
-        public static void Main()
+        [STAThread]
+        public static void Main(string[] args)
         {
-            ServiceBase.Run(new CalculatorWindowsService());
-        }
-        // Start the Windows service.
-        protected override void OnStart(string[] args)
-        {
-            if (serviceHost != null)
-            {
-                serviceHost.Close();
-            }
+            var file = Assembly.GetExecutingAssembly().ManifestModule.Name;
+            string app = null;
 
-            // Create a ServiceHost for the CalculatorService type and 
-            // provide the base address.
-            // serviceHost = new ServiceHost(typeof(CalculatorService.CalculatorService));
-            serviceHost = new ServiceHost(typeof(CalculatorService.CalculatorService),new Uri("http://localhost:8000/ServiceModelSamples/Service.svc"));
-            if (serviceHost != null)
+            try
             {
-                // Open the ServiceHost to create listeners and start listening for messages.
-                serviceHost.Open();
+                app = Path.GetFileNameWithoutExtension(file);
+                app = ServiceName;
+            }
+            catch (ArgumentException)
+            {
+            }
+            var serviceName = ServiceName;
+            var serviceStarting = IsInstalled(ServiceName);
+
+            if (!serviceStarting)
+            {
+                var attrs = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                var sTitle = ((AssemblyTitleAttribute) attrs[0]).Title;
+                if (args.Length >= 1)
+                {
+                    var commandLine = new Arguments(args);
+                    var user = commandLine["user"];
+                    var pwdc = commandLine["pwdc"];
+                    var pwd = commandLine["pwd"];
+
+                    //if (!String.IsNullOrEmpty(pwdc) && String.IsNullOrEmpty(pwd))
+                    //    pwd = Crypt.Decrypt(pwdc);
+                    ////MessageBox.Show("Password :" + pwd);
+                    //if (args[0].ToLower().Contains("/c"))
+                    //{
+                    //    var pwdTxt = Crypt.Encrypt(pwd);
+                    //    Clipboard.SetText(pwdTxt);
+                    //    MessageBox.Show(String.Format("Le mot de passe crypté est [{0}], celui-ci est disponibe dans le presse-papier", pwdTxt), "Cryptage mot de passe", MessageBoxButtons.OK);
+                    //    return;
+                    //}
+
+
+                    try
+                    {
+                        if (!_isInstalled && args[0].ToLower().Contains("/i"))
+                        {
+                            SelfInstaller.InstallMe(ServiceName, DisplayName, Comment, user, pwd);
+                            UninstallService(); // Désinstallation du service
+                        }
+                        else if (args[0].ToLower().Contains("/ui"))
+                        {
+                            var dr = MessageBox.Show($@"Voulez-vous réinstaller le service {sTitle} ?",
+                                sTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                            if (dr == DialogResult.Yes)
+                            {
+                                if (_isInstalled)
+                                    SelfInstaller.InstallMe(ServiceName, DisplayName, Comment, user, pwd);
+                                //On regarde à nouveau si le service est lancé
+                                serviceStarting = IsInstalled(serviceName);
+                                var bInstall = SelfInstaller.InstallMe(ServiceName, DisplayName, Comment, user, pwd);
+                                MessageBox.Show(
+                                    $@"La réinstallation du service {sTitle} a {(bInstall ? "réussi" : "échoué")}.",
+                                    sTitle, MessageBoxButtons.OK,
+                                    bInstall ? MessageBoxIcon.Information : MessageBoxIcon.Stop);
+                            }
+                            serviceStarting = IsInstalled(serviceName);
+                            if (_isInstalled)
+                            {
+                                dr = MessageBox.Show($@"Voulez-vous démarrer le service {sTitle} ?",
+                                    sTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                                if (dr == DialogResult.Yes)
+                                {
+                                    //Start service
+                                    var rv = ServicesManagerHelper.StartService(app);
+                                    MessageBox.Show(
+                                        $@"Le démarrage du service {sTitle} a {
+                                                (rv == ServicesManagerHelper.ReturnValue.Success ? "réussi" : "échoué")
+                                            }.",
+                                        sTitle, MessageBoxButtons.OK,
+                                        rv == ServicesManagerHelper.ReturnValue.Success
+                                            ? MessageBoxIcon.Information
+                                            : MessageBoxIcon.Stop);
+                                }
+                            }
+                        }
+                        else if (_isInstalled)
+                        {
+                            if (args[0].ToLower().Contains("/u"))
+                            {
+                                ServicesManagerHelper.StopService(app);
+                                SelfInstaller.UninstallMe(ServiceName);
+                            }
+                        }
+                    }
+                    catch (ArgumentNullException)
+                    {
+                    }
+
+                    try
+                    {
+                        if (_isInstalled && args[0].ToLower().Contains("/r"))
+                        {
+                            var rkHKLM = Registry.LocalMachine;
+                            var rkService = rkHKLM.OpenSubKey($@"SYSTEM\CurrentControlSet\services\{app}");
+                            var fullName = Assembly.GetExecutingAssembly().ManifestModule.FullyQualifiedName;
+                            var folderService = rkService.GetValue("ImagePath").ToString().Replace("\"", "");
+
+                            if (IsUpgradVersion(fullName, folderService))
+                                if (folderService != null && fullName != folderService)
+                                {
+                                    var rv = ServicesManagerHelper.StartService(app);
+                                    if (rv == ServicesManagerHelper.ReturnValue.ServiceAlreadyRunning)
+                                        ServicesManagerHelper.StopService(app);
+
+                                    if (File.Exists(folderService))
+                                        File.Delete(folderService);
+                                    File.Copy(fullName, folderService);
+                                }
+                                else
+                                {
+                                    SelfInstaller.InstallMe(ServiceName, DisplayName, Comment, user, pwd);
+                                }
+                        }
+                    }
+                    catch (ArgumentNullException)
+                    {
+                    }
+                    try
+                    {
+                        switch (commandLine["OPTION"])
+                        {
+                            case "start":
+                                ServicesManagerHelper.StartService(app);
+                                break;
+                            case "stop":
+                                ServicesManagerHelper.StopService(app);
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($@"Service {ex.Message}", sTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    #region isInstalled
+
+                    if (_isInstalled)
+                    {
+                        var dr = MessageBox.Show($@"Voulez-vous désinstaller le service {sTitle} ?",
+                            sTitle,
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dr == DialogResult.Yes)
+                        {
+                            var bInstall = SelfInstaller.UninstallMe(ServiceName);
+                            MessageBox.Show(
+                                $@"La désinstallation du service {sTitle} a {(bInstall ? "réussi" : "échoué")}.",
+                                sTitle, MessageBoxButtons.OK,
+                                bInstall ? MessageBoxIcon.Information : MessageBoxIcon.Stop);
+                        }
+                    }
+                    else
+                    {
+                        var dr =
+                            MessageBox.Show($@"Voulez-vous installer le service {sTitle} ?",
+                                sTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dr == DialogResult.Yes)
+                        {
+                            var bInstall = SelfInstaller.InstallMe(ServiceName, DisplayName, Comment, string.Empty, string.Empty);
+                            MessageBox.Show(
+                                $@"L’installation du service {sTitle} a {(bInstall ? "réussi" : "échoué")}.",
+                                sTitle, MessageBoxButtons.OK,
+                                bInstall ? MessageBoxIcon.Information : MessageBoxIcon.Stop);
+                        }
+                        serviceStarting = IsInstalled(serviceName);
+                        if (_isInstalled)
+                        {
+                            dr = MessageBox.Show($@"Voulez-vous démarrer le service {sTitle} ?",
+                                sTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                            if (dr == DialogResult.Yes)
+                            {
+                                //Start service
+                                var rv = ServicesManagerHelper.StartService(app);
+                                MessageBox.Show(
+                                    $@"Le démarrage du service {sTitle} a {
+                                            (rv == ServicesManagerHelper.ReturnValue.Success ? "réussi" : "échoué")
+                                        }.",
+                                    sTitle, MessageBoxButtons.OK,
+                                    rv == ServicesManagerHelper.ReturnValue.Success
+                                        ? MessageBoxIcon.Information
+                                        : MessageBoxIcon.Stop);
+                            }
+                        }
+                    }
+
+                    #endregion
+                }
+            }
+            else
+            {
+                try
+                {
+                    var servicestorun = new ServiceBase[] {new Service()};
+                    ServiceBase.Run(servicestorun);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($@"Message {ex.Message},{ex?.InnerException?.Message}");
+                }
             }
         }
 
-        protected override void OnStop()
+        private static bool IsInstalled(string serviceName)
         {
-            if (serviceHost != null)
+            var serviceStarting = false;
+            _isInstalled = false;
+            var services = new ServiceController[] { };
+            try
             {
-                serviceHost.Close();
-                serviceHost = null;
+                services = ServiceController.GetServices();
+            }
+            catch (Win32Exception)
+            {
+            }
+            foreach (var service in services)
+                try
+                {
+                    if (service.ServiceName.Equals(serviceName))
+                    {
+                        _isInstalled = true;
+                        if (service.Status == ServiceControllerStatus.StartPending)
+                            serviceStarting = true;
+                        break;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                }
+            return serviceStarting;
+        }
+
+        private static bool IsUpgradVersion(string fileSource, string fileTarget)
+        {
+            if (fileTarget == null)
+                return true;
+
+            var source = FileVersionInfo.GetVersionInfo(fileSource);
+            var target = FileVersionInfo.GetVersionInfo(fileTarget);
+
+            if (source.FileMajorPart > target.FileMajorPart)
+                return true;
+            if (source.FileMajorPart == target.FileMajorPart && source.FileMinorPart > target.FileMinorPart)
+                return true;
+            if (source.FileMajorPart == target.FileMajorPart && source.FileMinorPart == target.FileMinorPart &&
+                source.FileBuildPart > target.FileBuildPart)
+                return true;
+            if (source.FileMajorPart == target.FileMajorPart && source.FileMinorPart == target.FileMinorPart &&
+                source.FileBuildPart == target.FileBuildPart && source.FileBuildPart > target.FileBuildPart)
+                return true;
+            return false;
+        }
+
+        private static void UninstallService()
+        {
+            var services = new ServiceController[] { };
+            try
+            {
+                services = ServiceController.GetServices();
+            }
+            catch (Win32Exception)
+            {
+            }
+            foreach (var service in services)
+                try
+                {
+                    if (service.ServiceName.Equals(ServiceName))
+                    {
+                        if (service.Status == ServiceControllerStatus.Running ||
+                            service.Status == ServiceControllerStatus.StartPending)
+                            ServicesManagerHelper.StopService(ServiceName);
+
+                        UninstallService(ServiceName);
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                }
+        }
+
+        private static ServicesManagerHelper.ReturnValue UninstallService(string svcName)
+        {
+            var objPath = $@"Win32_Service.Name='{svcName}'";
+            using (var service = new ManagementObject(new ManagementPath(objPath)))
+            {
+                try
+                {
+                    var outParams = service.InvokeMethod("delete", null, null);
+                    return (ServicesManagerHelper.ReturnValue) Enum.Parse(typeof(ServicesManagerHelper.ReturnValue),
+                        outParams["ReturnValue"].ToString());
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message.ToLower().Trim() == "not found" || ex.GetHashCode() == 41149443)
+                        return ServicesManagerHelper.ReturnValue.ServiceNotFound;
+                    throw ex;
+                }
             }
         }
     }
---
+++ WindowsService/Service.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    internal partial class Service : ServiceBase
+    {
+        private ServiceHost _serviceHost;
+
+        public Service()
+        {
+            this.ServiceName = "WCFWindowsServiceSample";
+        }
+
+        // Start the Windows service.
+        protected override void OnStart(string[] args)
+        {
+            _serviceHost?.Close();
+
+            // Create a ServiceHost for the CalculatorService type and 
+            // provide the base address.
+            // serviceHost = new ServiceHost(typeof(CalculatorService.CalculatorService));
+            _serviceHost = new ServiceHost(typeof(CalculatorService.CalculatorService),
+                new Uri("http://localhost:8000/ServiceModelSamples/Service.svc"));
+            _serviceHost?.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (_serviceHost != null)
+            {
+                _serviceHost.Close();
+                _serviceHost = null;
+            }
+        }
+    }
+}
